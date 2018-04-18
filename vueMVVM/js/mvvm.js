4c0beee8a47b289411bfd7fc8ee7bb35a6e0c50d
jianwei <391473403@qq.com>
2018-04-18 17:47:05 +0800

    add Observer Dep

--- vueMVVM/js/mvvm.js
+++ vueMVVM/js/mvvm.js
@@ -1,70 +1,81 @@
-function Mvvm(options = {}) {
-  this.$options = options
-  let data = this._data = this.$options.data
-  obServer(data) // 数据劫持
-  new Compile(this.$options.el, this)
+let aab = {
+  name: 'jianwei'
 }
 
 function ObServer(data) {
-  for (let key in data) {
-    let val = data[key];
-    obServer(val);
+  console.log(this)
+  this.data = data
+  // console.log(this)
+  this.walk(data)
+}
+
+ObServer.prototype = {
+  walk: function (data) {
+    let me = this
+    Object.keys(this.data).forEach(function (key) {
+      me.convert(key, data[key])
+    })
+  },
+  convert: function (key, val) {
+    this.defineReactive(this.data, key, val)
+  },
+  defineReactive: function (data, key, val) {
+    let dep = new Dep()
+    let childObj = obServer(val)
     Object.defineProperty(data, key, {
-      configurable: false,
       enumerable: true,
-      get() {
-        console.log(val, 'get')
-        // return val;
-        return this._data[key] // this.a = {b:1}
+      configurable: false,
+      get: function () {
+        // 
+        if (Dep.target) {
+          dep.depend()
+        }
+        console.log('劫持数据')
+        return val
       },
-      set(newVal) {
-        // console.log('set')
-        // if (val === newVal) return;
-        // val = newVal;
-        // obServer(newVal);
-        this._data[key] = newVal;
+      set: function (newVal) {
+        if (val === newVal) return
+        console.log('检查数据改变--->newVal：' + newVal)
+        val = newVal
+        // 新的值是object的话，进行监听
+        childObj = obServer(newVal)
+        // 通知订阅者
+        dep.notify()
       }
     })
   }
 }
-console.log(Mvvm.data)
 
 function obServer(data) {
   if (!data || typeof data !== 'object') return
-  return new ObServer(data);
+  return new ObServer(data)
 }
 
-function Compile(el, vm) {
-  // 将el挂载到实列上方便使用
-  vm.$el = document.querySelector(el);
-  // console.log(vm.$el.firstChild)
-  // 创建文档碎片
-  let fragment = document.createDocumentFragment();
-  while (child = vm.$el.firstChild) {
-    fragment.appendChild(child); // 此时将el中的内容放入内存中
-  }
-  // fragment.appendChild(vm.$el)
-  console.log(fragment);
+var uid = 0
 
-  function replace(frag) {
-    // console.log(Array.from(frag.childNodes));
-    Array.from(frag.childNodes).forEach(node => {
-      // console.log(node.childNodes.length)
-      let txt = node.textContent;
-      let reg = /\{\{(.*?)\}\}/g; // 正则匹配{{}}
-      if (node.nodeType === 3 && reg.test(txt)) {
-        console.log(RegExp.$1)
-        let arr = RegExp.$1.split('.')
-        console.log(arr)
-        let val = vm
-        arr.forEach(key => {
-          val = val[key]
-        })
-      }
-      if (node.childNodes && node.childNodes.length) {
-        replace(node)
-      }
+function Dep() {
+  this.id = uid++
+    this.subs = []
+}
+
+Dep.prototype = {
+  addsub: function (sub) {
+    this.subs.push(sub)
+  },
+  depend: function () {
+    Dep.target.addDep(this)
+  },
+  removeSub: function (sub) {
+    let index = this.subs.indexOf(sub)
+    if (index !== -1) {
+      this.subs.splice(index, 1)
+    }
+  },
+  notify: function () {
+    this.subs.foreach(function (sub) {
+      sub.update()
     })
   }
-  replace(fragment)
 }
+
+Dep.target = null
